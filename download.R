715a0e016b4d5a20750d4d8894a0f02d050cbfa0
Martin Isaksson <martin.isaksson@gmail.com>
2014-08-13 22:14:25 +0200

    Plots 1 and 2

--- download.R
+++ download.R
@@ -10,3 +10,29 @@ if (!file.exists(filename)){
 ## This first line will likely take a few seconds. Be patient!
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
+a <- aggregate(data=NEI, Emissions~year+Pollutant, FUN=sum)
+
+png('plot1.png', width=480, height=480, units="px", pointsize=20)
+
+plot(a$year, a$Emissions, 
+	xlab="Year", 
+	ylab="PM2.5 emission", 
+	main="Total PM2.5 emission from all sources",
+	pty=2)
+
+dev.off()
+
+b <- aggregate(data=NEI[NEI$fips == "24510",], Emissions~year+Pollutant, FUN=sum)
+
+png('plot2.png', width=480, height=480, units="px", pointsize=20)
+
+plot(b$year, b$Emissions, 
+	xlab="Year", 
+	ylab="PM2.5 emission in Baltimore", 
+	main="Total PM2.5 emission from all sources",
+	pty=2)
+
+dev.off()
+
+
