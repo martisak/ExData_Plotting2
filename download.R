e36b5eaf85d7b0f34b03a22253e85a724c8fffbe
Martin Isaksson <martin.isaksson@gmail.com>
2014-08-13 21:54:39 +0200

    Download
    Download.R to download and read data

---
+++ download.R
@@ -0,0 +1,12 @@
+data_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+filename <- "exdata_data_FNEI.zip"
+
+if (!file.exists(filename)){
+  cat("File did not exist, downloading...\n")
+  download.file(data_url, filename, method="curl")
+  unzip(filename)
+}
+
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
